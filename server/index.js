e29e04dab274df8158826c29419d685db9f46afe
Rick_Paramanik <rick.paramanik@faceacademy.in>
2023-06-23 12:02:10 +0530

    basic server setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('blog app server index page')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const router = require("./router/router");
+
+app.use(express.json());
+app.use('/api',router)
+
+app.listen(process.env.PORT, console.log(`LISTENING TO PORT ${process.env.PORT}`));
+
+app.get('/',(req,res)=>{
+    res.send("response from blog-app server")
+})
