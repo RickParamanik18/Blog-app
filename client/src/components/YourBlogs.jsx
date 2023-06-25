9c51f6c53a61f3244fad0653bef4773ab9b5d5e6
Rick_Paramanik <rick.paramanik@faceacademy.in>
2023-06-25 07:46:09 +0530

    homepage complete

--- client/src/components/YourBlogs.jsx
+++ client/src/components/YourBlogs.jsx
@@ -1,6 +1,24 @@
-const YourBlogs = ()=>{
+import styles from "../styles/yourblogs.module.css";
+import { Link } from "react-router-dom";
+
+const YourBlogs = () => {
     return (
-        <div>Your Blogs</div>
-    )
-}
-export default YourBlogs
+        <div>
+            <div className={styles.container}>
+                <div className="h3">Your Blogs</div>
+                <div className={styles.blogs}>
+                    <div className={styles.blog_link}>
+                        <Link to="/">This is a log heading about a blog</Link>
+                    </div>
+                    <div className={styles.blog_link}>
+                        <Link to="/">This is a log heading about a blog</Link>
+                    </div>
+                </div>
+                <div className={styles.new_blog}>
+                    <Link to="/">+ New</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default YourBlogs;
